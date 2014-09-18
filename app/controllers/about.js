3c944340ae30d9653726360dee17a23c2689a2c6
Blake Mason & Nick Bernard <student@epicodus.com>
2014-09-18 16:16:40 -0700

    update profiles controller and template

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   isPictureShowing: false,
   actions: {
     showRealName: function() {
-      alert("Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Vlad III the Impaler.")
+      alert("Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Vlad III the Impaler.");
     },
     showPicture: function() {
       this.set('isPictureShowing', true);
---
+++ app/controllers/profiles.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  // sortProperties: ['date', 'time'],
+  actions: {
+    sortByTitle: function() {
+      this.set('sortProperties', ['title'])
+    }
+  }
+})
